3a4f2b66d24686d1c1b193c1357742434e6d3e19
bowski43 <43mngt@gmail.com>
2024-04-08 13:19:14 +0300

    Initial Commit

---
+++ ServiceDesk/ServiceDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceDeskApp.swift
+//  ServiceDesk
+//
+//  Created by Serge Broski on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ServiceDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
